cfd5c32c0e7db93bfdd5ade862b39c330b641134
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2022-04-26 10:43:45 -0300

    add basic exceptions

--- cantina-do-ifal/src/cantina/Estoque.java
+++ cantina-do-ifal/src/cantina/Estoque.java
@@ -5,7 +5,7 @@ public class Estoque {
 	private int index = 0;
 	
 	public void cadastrarItem(String nome, String desc, double preco_compra, 
-			double preco_venda, int quantidade_comprada, int quantidade_vendida) 
+			double preco_venda, int quantidade_comprada, int quantidade_vendida) throws ValorInvalidoException 
 	{
 		this.itens[index] = new Item(nome, desc, preco_compra, preco_venda, quantidade_comprada, quantidade_vendida);
 		index++;
--- cantina-do-ifal/src/cantina/Item.java
+++ cantina-do-ifal/src/cantina/Item.java
@@ -9,20 +9,26 @@ class Item {
 	private int quantidade_vendida;
 	
 	Item(String nome, String desc, double preco_compra, double preco_venda, 
-			int quantidade_comprada, int quantidade_vendida) 
+			int quantidade_comprada, int quantidade_vendida) throws ValorInvalidoException 
 	{
+		if (preco_venda < preco_compra) {
+			throw new ValorInvalidoException("Preço de venda menor que o de compra.");
+		}
+		
+		if (preco_venda < 0 || preco_compra < 0) {
+			throw new ValorInvalidoException("Preço de venda ou compra menores do que zero.");
+		}
+		
+		if (quantidade_comprada < 0 || quantidade_vendida < 0) {
+			throw new ValorInvalidoException("Quantidade de compra ou venda menores do que zero.");
+		}
+		
 		this.nome = nome; 
 		this.desc = desc;
 		this.preco_compra = preco_compra;
 		this.preco_venda = preco_venda;
 		this.quantidade_comprada = quantidade_comprada;
 		this.quantidade_vendida = quantidade_vendida;
-	}
-	
-	
-	
-	public void setQuantidade(int quantidade_comprada) {
-		this.quantidade_comprada = quantidade_comprada;
 	}
 	
 }
