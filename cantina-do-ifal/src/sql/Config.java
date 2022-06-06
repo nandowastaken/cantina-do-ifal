58034e4f675ce0b9e8eb09f36694055513a30991
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2022-06-06 15:06:02 -0300

    Delete cantina-do-ifal/src/sql directory

--- cantina-do-ifal/src/sql/Config.java
+++
@@ -1,9 +0,0 @@
-package sql;
-
-public class Config {
-    public static String database = "cantina";
-    public static String urlMySQL = "jdbc:mysql://localhost/"+database;
-
-    public static String user = "root"; 
-    public static String pass = "root"; // change the password to yours 
-}
